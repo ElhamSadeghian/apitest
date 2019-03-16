11ef2e9a7a94eed7142d41f6cda52152e9613c71
Elham Sadeghian <elhamsadeghian72@gmail.com>
2019-03-16 21:36:38 +0330

    changing app/User.php

--- app/User.php
+++ app/User.php
@@ -5,19 +5,23 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+     protected $table = 'users';
     protected $fillable = [
-        'name', 'email', 'password',
+        'fullName', 'email', 'password', 'phone', 'avatar', 'birthDate', 'registerDate', 'gender', 'zipCode'
     ];
+
+    public $timestamps = false;
 
     /**
      * The attributes that should be hidden for arrays.
