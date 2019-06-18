d3ea30cc590da508c4be4c3fa4178a558ce3b391
ElhamSadeghian <elhamsadeghian72@gmail.com>
2019-06-18 14:11:18 +0430

    added download route

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::get('/main', 'MainController@main');
 Route::get('users', 'UsersController@get');
 Route::get('users/{id}', 'UsersController@getOneUser');
 Route::post('users', 'UsersController@regist');
-Route::put('users/{id}', 'UsersController@update');
+Route::post('users/{id}', 'UsersController@update');
 Route::delete('users/{id}', 'UsersController@delete');
 
 Route::post('users/login', 'UsersController@login');
@@ -35,6 +35,9 @@ Route::get('users/login1', 'UsersController@login1')->name('login');
 Route::post('users/test', 'UsersController@test');
 Route::get('curl', 'UsersController@curl');
 
+
+
 Route::group(['middleware' => 'auth:api'], function(){
     Route::get('users/details', 'UsersController@details');
+    Route::get('download', 'UsersController@download');
 });
